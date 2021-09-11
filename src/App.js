560e9cbb43de1e3d0656ba6f452348c68abb5db0
Elyssa Chou <nutcasealpha@gmail.com>
2021-09-11 17:02:33 -0400

    initial react commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import ChallengeList from './components/challengeList/ChallengeList';
+import ChallengeDetail from './components/challengeDetail/ChallengeDetail'
+import Chat from './components/chat/Chat';
+import Session from './components/session/Session';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+        <Route path='/' exact>
+          <Redirect to='/challengelist'/>
+        </Route>
+
+        <Route path='/challengelist' exact>
+           <ChallengeList></ChallengeList>
+        </Route>
+
+        <Route path='/challengeDetail/:id' exact>
+           <ChallengeDetail></ChallengeDetail>
+        </Route>
+
+        <Route path='/session/:id' exact>
+           <Session></Session>
+        </Route>
+
+        <Route path='/chat/:id' exact>
+           <Chat></Chat>
+        </Route>
+    </Switch>
   );
 }
 
